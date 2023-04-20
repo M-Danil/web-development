dbbb951893f9915e793324b756a6b6d36c39ae5a
M-Danil <stud143728@vyatsu.ru>
2023-04-20 22:21:00 +0300

    Danil
    Залил 1 лабу

---
+++ lab1/Typescript/экспорт и импорт/main.ts
@@ -0,0 +1,11 @@
+import { returnGreeting } from './greetings_module.js';         // imports a single function in the module
+import * as allGreetingFunctions from './greetings_module.js';  // imports all exported components in the module
+import { returnGreeting as returnGreetingLength } from './greetings-utilities_module.js';
+import dotenv from 'dotenv';
+
+
+const result = dotenv.config();
+
+returnGreeting('Hola!')  // Displays 'The message from Greetings_module is Hola!'
+allGreetingFunctions.returnGreeting('Bonjour');  // Displays 'The message from Greetings_module is Bonjour!'
+returnGreetingLength('Ciao!');  // Displays 'The message from GreetingsWithLength_module is Ciao! It is 5 characters long.'
